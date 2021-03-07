7b8e550e344fd8d34b929e5852d9af5f58025ee5
P.M.SALMAN KHAN <67056107+SALMANKHANPM@users.noreply.github.com>
2021-03-07 21:33:01 +0530

    vendor : go vanilla

--- config/common.mk
+++ config/common.mk
@@ -137,17 +137,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.theme_id=5 \
     ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
 
-# SetupWizard configuration
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.feature.baseline_setupwizard_enabled=true \
-    ro.setupwizard.enterprise_mode=1 \
-    ro.setupwizard.rotation_locked=true \
-    setupwizard.enable_assist_gesture_training=true \
-    setupwizard.theme=glif_v3_light \
-    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
-    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
-    setupwizard.feature.show_pixel_tos=false
-
 # StorageManager configuration
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.storage_manager.show_opt_in=false
@@ -248,13 +237,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Audio
 $(call inherit-product, vendor/cherish/config/audio.mk)
-
-ifeq ($(CHERISH_NONGAPPS), true)
-include vendor/cherish/config/basicapps.mk
-else
-# GApps
-$(call inherit-product, vendor/gapps/config.mk)
-endif
 
 # OTA
 $(call inherit-product, vendor/cherish/config/ota.mk)
